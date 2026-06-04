632c527936231cb7dd51915c42624d0c64735dbb
Pika28samson <56716449+Pika28samson@users.noreply.github.com>
2026-06-04 11:36:53 +0100

    Refactor to use async/await for API call

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const axios = require("axios");
 
-axios
-  .get("https://generativelanguage.googleapis.com")
-  .then(r => console.log(r.status))
-  .catch(console.error);
+async function test() {
+
+    const response = await axios.post(
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
+        {
+            contents: [
+                {
+                    parts: [
+                        {
+                            text: "Hello"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            headers: {
+                "x-goog-api-key": process.env.GEMINI_API_KEY,
+                "Content-Type": "application/json"
+            }
+        }
+    );
+
+    console.log(response.data);
+}
+
+test().catch(err => {
+    console.log(err.response?.data || err);
+});
